ce5cd87239b1d5f64e4e7dc97fd92fe72048e2ed
Dishugarg <95537185+Dishugarg@users.noreply.github.com>
2022-10-10 20:33:16 +0530

    added files

---
+++ main.py
@@ -0,0 +1,14 @@
+def locate_cards(cards,query):
+    start = 0
+    end = len(cards)-1
+    mid = divmod(start+end, 2)[0]
+    while cards[getlocation] ==
+
+if __name__ == '__main__':
+    test = {
+        'input': {
+            'cards': [1, 2, 3, 4, 5, 6, 7, 8],
+            'query': 5
+        },
+        'output': 4
+    }
